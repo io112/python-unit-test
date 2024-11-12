fc7fca25d65ec0df50193c6d6127854778f006a7
Dmitry Paramono <paramonov@informatics.ru>
2024-11-12 18:21:30 +0300

    add tests

--- test_main.py
+++ test_main.py
@@ -7,4 +7,4 @@ class MinOfThreeVarsTestCase(unittest.TestCase):
 
     def test_min_a(self):
         res = min_of_three_vars(1, 2, 3)
-        self.assertEqual(res, 2)
+        self.assertEqual(res, 1)
